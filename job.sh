2c83336c2620adb59f694af759a77fc010e0864b
Daniele Piano <dpiano01@gmail.com>
2024-06-03 14:37:53 +0200

    slurm, singularity :: 1 slurm process; restore 2 processors, update paths with /Users/danielepiano

--- job.sh
+++ job.sh
@@ -4,9 +4,9 @@
 #SBATCH --output=mx-multiplication.out
 #SBATCH --error=mx-multiplication.err
 #SBATCH --time=00:10
-#SBATCH --ntasks=2
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=100M
 
-module load singularity
-mpiexec -np 2 singularity run mx-multiplication.sif /bin/mx-multiplication
+#module load singularity
+/usr/mpi/gcc/openmpi-4.0.4rc3/bin/mpirun -np 1 singularity run mx-multiplication.sif /bin/mx-multiplication
