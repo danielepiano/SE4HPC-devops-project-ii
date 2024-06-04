b7c039479c57b1d6b2ec8b6d98fdaa60ee3fa8b9
Daniele Piano <dpiano01@gmail.com>
2024-06-04 15:33:02 +0200

    slurm :: remove mpirun; fix config; fix success message (still double output log :/ ???)

--- job.sh
+++ job.sh
@@ -8,9 +8,9 @@
 #SBATCH --mail-user=dpiano01@gmail.com
 
 #SBATCH --time=00:10
-#SBATCH --nodes=2
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=3
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=100M
 
 #module load singularity
@@ -32,17 +32,17 @@ echo "$(date -d @${start} "+%Y-%m-%d %H:%M:%S"): ${SLURM_JOB_NAME} start id=${SL
 # Send email about begin, success, failure via e-mail (Slurm feature)
 # Send webhook to enable GitHub Action workflow to send result/failure notification via Telegram
 
-if ( srun mpirun -np 2 singularity run --bind /g100_scratch/usertrain/a08trb53:/scratch_local mx-multiplication.sif ); \
+if ( srun singularity run --bind /g100_scratch/usertrain/a08trb53:/scratch_local mx-multiplication.sif ); \
   then ( \
     echo "$SLURM_JOB_NAME Ended after $(secs_to_human $(($(date +%s) - ${start}))) id=$SLURM_JOB_ID" \
-    && JSON_CONTENT=$(sed ':a;N;$!ba;s/\n/\\n/g' output.txt | sed ':a;N;$!ba;s/\n/\\n/g') \
+    && JSON_CONTENT=$(head -n 4 output.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g') \
     && curl -L \
       -X POST \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer $GITHUB_TOKEN" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
       $WEBHOOK_URL \
-      -d "{\"event_type\": \"result-notification\", \"client_payload\": {\"passed\": true, \"log\": \"$JSON_CONTENT\" \"filename\": \"output.txt\"}}"
+      -d "{\"event_type\": \"result-notification\", \"client_payload\": {\"passed\": true, \"log\": \"$JSON_CONTENT...\", \"filename\": \"output.txt\"}}"
   ); \
   else ( \
     JSON_CONTENT=$(head -n 10 error.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g') \
