ff40a3dac5cb4e63943fedaf753913ccbd93d556
Daniele Piano <dpiano01@gmail.com>
2024-06-04 02:48:55 +0200

    slurm, singularity :: try moving mpirun on slurm

--- job.sh
+++ job.sh
@@ -4,9 +4,10 @@
 #SBATCH --output=output.txt
 #SBATCH --error=error.txt
 #SBATCH --time=00:10
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=100M
 
 #module load singularity
-srun singularity run mx-multiplication.sif
+srun /usr/mpi/gcc/openmpi-4.0.4rc3/bin/mpirun -np 2 singularity run --bind /g100_scratch/usertrain/a08trb53:/scratch_local mx-multiplication.sif
